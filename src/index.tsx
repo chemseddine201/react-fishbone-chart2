a365e3e5e9a36f488d2400896f54406ee9ad2a5b
chemseddine201 <chemseddine.zara@gmail.com>
2025-02-26 01:03:05 +0100

    revert svg icons

--- src/index.tsx
+++ src/index.tsx
@@ -238,7 +238,7 @@ class FishboneChart extends Component<FishboneChartProps, FishboneChartState> {
         this.props.onInitStart();
       }
       
-      const timer = setTimeout(() => {
+      setTimeout(() => {
         try {
           new FishboneDrawer().init();
           
@@ -445,6 +445,29 @@ class FishboneChart extends Component<FishboneChartProps, FishboneChartState> {
                 width='150px'
                 height='150px'
               >
+                <g transform='translate(0,2600) scale(0.1,-0.1)' fill={colorValue} stroke='none'>
+                  <path
+                    d='M11115 23116 c-1317 -567 -2786 -2560 -4292 -5825 -1362 -2952 -2754
+                -6983 -3772 -10918 -186 -719 -491 -2012 -491 -2083 0 -30 344 -344 610 -556
+                1162 -929 2703 -1647 4565 -2128 2659 -686 5771 -811 8592 -346 2456 405 4519
+                1226 5903 2349 252 204 530 462 530 490 0 25 -201 819 -290 1146 -968 3559
+                -2450 7107 -4408 10555 -1283 2259 -2801 4475 -4133 6032 -509 595 -1011 1102
+                -1290 1303 l-46 33 -37 -151 c-134 -540 -512 -1451 -941 -2267 -195 -372 -542
+                -987 -551 -978 -2 2 19 170 47 374 183 1350 238 2135 188 2674 -10 108 -45
+                315 -57 336 -5 9 -42 -3 -127 -40z m3330 -7315 c544 -115 902 -567 985 -1245
+                14 -111 14 -404 0 -511 -87 -682 -426 -1188 -905 -1352 -106 -36 -224 -53
+                -367 -53 -496 0 -911 158 -1182 451 -164 178 -274 401 -328 672 -30 146 -32
+                448 -5 597 41 228 85 358 187 565 100 200 183 318 330 465 259 259 531 390
+                900 434 73 9 298 -5 385 -23z'
+                  />
+                  <path
+                    d='M14010 15243 c-198 -33 -391 -132 -530 -272 -268 -269 -382 -679
+                -301 -1076 43 -210 156 -406 303 -525 72 -57 212 -129 308 -156 99 -29 413
+                -26 525 4 242 65 431 227 537 459 119 264 137 688 43 1057 -25 102 -79 238
+                -100 256 -10 8 -65 -60 -261 -325 -137 -185 -250 -332 -251 -328 -1 4 -36 188
+                -78 408 -41 220 -80 424 -86 453 -10 52 -11 52 -47 51 -20 -1 -48 -4 -62 -6z'
+                  />
+                </g>
               </svg>
             </div>
           ) : (
@@ -459,7 +482,7 @@ class FishboneChart extends Component<FishboneChartProps, FishboneChartState> {
       <Fragment>
         {this.state.showSkeleton ? (
           <div className={`fish-tail ${!this.state.isLoading ? '' : 'hidden'}`}>
-            <svg
+              <svg
               className='fish-tail-svg'
               fill={this.getColorValue()}
               version='1.1'
@@ -469,6 +492,15 @@ class FishboneChart extends Component<FishboneChartProps, FishboneChartState> {
               viewBox='0 0 572 572'
               xmlSpace='preserve'
             >
+              <g>
+                <g>
+                  <path
+                    d='M117.518,296.042l333.161,272.132c8.286,6.646,12.062,3.941,8.43-6.04l-88.442-260.049
+              c-3.63-9.981-3.596-26.156,0.076-36.123l88.29-256.26c3.672-9.966-0.101-12.702-8.431-6.11L117.594,272.07
+              C109.265,278.661,109.231,289.395,117.518,296.042z'
+                  />
+                </g>
+              </g>
             </svg>
           </div>
         ) : null}
